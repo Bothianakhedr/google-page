5a0d6835a0c5b76e8da712548cf372baf7c0d66e
Bothianakhedr <Bothianakhedr198@gmail.com>
2025-02-04 22:37:52 +0200

    update style icom

--- src/component/GoogleContent/GoogleContent.jsx
+++ src/component/GoogleContent/GoogleContent.jsx
@@ -3,27 +3,27 @@ import googleImage from "../../assets/image/Googlelogo_color_272x92dp.png";
 export default function GoogleContent() {
   return (
     <>
-      <div className="img text-center my-4 ">
+      <div className="img text-center my-1 ">
         <img className="" src={googleImage} alt="google" />
       </div>
-      <div className="input-info  position-relative">
-        <div className="searchIcon position-absolute     ">
-          <i className="fa-solid fa-magnifying-glass  fs-6 text-secondary"></i>
+      <div className="input-info position-relative">
+        <div className="searchIcon position-absolute ">
+          <i className="fa-solid fa-magnifying-glass    "></i>
         </div>
         <input
           type="text"
           // placeholder="search Google or type a URL"
-          className="ps-5 search-input  w-50  rounded-5 p-3 mx-auto border-0 text-secondary d-flex justify-content-between"
+          className="ps-5 search-input   rounded-5 p-3 mx-auto border-0  d-flex justify-content-between"
         />
         <div className="searchIconsBox d-flex position-absolute">
           <div className="searchByVoice position-relative ">
-            <i className=" fa fa-microphone text-secondary"></i>
+            <i className=" fa fa-microphone  "></i>
             <span className="  border-2 position-absolute ">
               search by voice
             </span>
           </div>
           <div className="searchByImage position-relative">
-            <i className="fa-regular fa-image ms-3 text-secondary "></i>
+            <i className="fa-regular fa-image ms-3  "></i>
             <span className="  border-2 position-absolute ">
               search by image
             </span>
